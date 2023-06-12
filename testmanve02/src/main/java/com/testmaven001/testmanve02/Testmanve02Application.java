649863f142836a2eacaf03877cdd7de0174e205f
shibobo <shibo0615@126.com>
2023-06-12 17:52:11 +0800

    222

--- testmanve02/src/main/java/com/testmaven001/testmanve02/Testmanve02Application.java
+++ testmanve02/src/main/java/com/testmaven001/testmanve02/Testmanve02Application.java
@@ -8,7 +8,7 @@ public class Testmanve02Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Testmanve02Application.class, args);
-        System.out.println("");
+        System.out.println("123");
     }
 
 }
